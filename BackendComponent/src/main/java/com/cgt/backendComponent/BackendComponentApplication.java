0290c3900595e3cad5d6d092be359f707a4f3ab2
stefancgt <61683523+stefancgt@users.noreply.github.com>
2021-03-04 09:35:00 +0100

    Initial app

---
+++ BackendComponent/src/main/java/com/cgt/backendComponent/BackendComponentApplication.java
@@ -0,0 +1,26 @@
+package com.cgt.backendComponent;
+
+import java.util.Properties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BackendComponentApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+		return springApplicationBuilder.sources(BackendComponentApplication.class).properties(getProperties());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendComponentApplication.class, args);
+	}
+
+	static Properties getProperties() {
+		Properties props = new Properties();
+		props.put("spring.config.additional-location", "/config/custom/application.properties");
+		return props;
+	}
+}
